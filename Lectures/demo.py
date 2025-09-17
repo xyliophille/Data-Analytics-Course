a588d3e59221094d5cbb958c7d3326b2cd93aac6
XYliophille <xyliophille06@gmail.com>
2025-09-18 00:29:41 +0530

    Delete Lectures directory

--- Lectures/demo.py
+++
@@ -1,7 +0,0 @@
-# def add(x,y):
-#     """This function adds two numbers."""
-#     return x + y
-
-# dictionary:
-# 1. Creating a dictionary to store employee details
-employee = {"name": "John", "age": 30, "gender": "Male"}
